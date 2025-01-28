f2064593401eb2ab2adbbee0cf4676be9d5423c6
arhaanhamid <suhamcareer@gmail.com>
2025-01-28 12:27:37 +0530

    Memories section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
+import Memories from "./components/Memories/Memories";
 function App() {
   return (
     <div className="main">
       <Nav />
       <Header />
+      <Memories />
     </div>
   );
 }
