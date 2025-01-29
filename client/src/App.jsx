cbf8cb6435b55222d71da8658e27a06905d45f96
arhaanhamid <suhamcareer@gmail.com>
2025-01-29 22:31:44 +0530

    example trip section, pending some kind of hover effect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Memories from "./components/Memories/Memories";
+import PopularTrip from "./components/PopularTrips/PopularTrip";
 function App() {
   return (
     <div className="main">
       <Nav />
       <Header />
       <Memories />
+      <PopularTrip />
     </div>
   );
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all files in `src` for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
